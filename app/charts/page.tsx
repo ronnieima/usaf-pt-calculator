de61b40b82b6037b9d1d8ee177317b24301cc4db
Ronnie <ronnie.imagawa@gmail.com>
2023-11-01 09:23:49 +0100

    add pagae routes

---
+++ app/charts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsPage = () => {
+  return <div>ChartsPage</div>;
+};
+
+export default ChartsPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>a</div>;
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-sky-800 to-indigo-800 p-5"></div>
+  );
 }
