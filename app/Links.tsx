2b6ed80ad2b23c2f3bdab7b6af0a1432b3d931ca
Ronnie <ronnie.imagawa@gmail.com>
2023-11-01 13:12:01 +0100

    add active styling browser

--- app/Links.tsx
+++ app/Links.tsx
@@ -1,14 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import { links } from "./NavBar";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+import { NavLink } from "@mantine/core";
 
 function Links() {
+  const currentPath = usePathname();
   return (
     <ul className="gap-16 text-2xl hidden lg:flex list-none">
       {links.map((link) => (
         <li key={link.href}>
           <Link
             href={link.href}
-            className="text-zinc-500 no-underline hover:text-zinc-800"
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500 no-underline": link.href !== currentPath,
+              "transition-colors hover:text-zinc-800 no-und": true,
+            })}
           >
             {link.label}
           </Link>
