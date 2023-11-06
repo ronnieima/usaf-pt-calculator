ea0cff5c560490fc2e7bdbccd49b9f9f5ca37db0
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-06 16:42:31 +0100

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🛩</text></svg>"
+        />
+      </head>
       <body>
         <ToastContainerWrapper />
         <NavBar />
