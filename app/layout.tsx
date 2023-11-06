92d3fecb494dbd81c9ab1b3afef1575ef06bec1a
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-06 17:39:37 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/ui/NavBar";
 import Footer from "./components/ui/Footer";
 import ToastContainerWrapper from "./components/ToastContainerWrapper";
 import { toast } from "react-toastify";
+import Script from "next/script";
 
 export const metadata = {
   title: "USAF PT Calculator",
@@ -22,6 +23,18 @@ export default function RootLayout({
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🛩</text></svg>"
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+        </Script>
       </head>
       <body>
         <ToastContainerWrapper />
