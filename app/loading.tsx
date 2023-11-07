5ad8a088e72967dee7baf88b3959acdeb332c8df
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-07 16:46:06 +0100

    fix loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import { MoonLoader } from "react-spinners";
 const loading = () => {
   return (
     <main className="flex min-h-screen flex-col items-center  justify-center gap-4 py-36 text-center text-zinc-300">
-      <MoonLoader size="50" color="white" />
+      <MoonLoader size={100} color="#FFF" />
       <p>Loading your page...</p>
     </main>
   );
