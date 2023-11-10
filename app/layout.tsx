650b7444b8c32da1a042beac6ef3fdd9805215f8
Ronnie <ronnie.imagawa@gmail.com>
2023-11-10 14:56:27 +0100

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,12 @@ import Script from "next/script";
 
 import { GeistSans } from "geist/font";
 
-export const metadata = {
-  title: "USAF PT Calculator",
-  description: "Calculate your PT score!",
+const metadata = {
+  title: "Calculate your USAF PT Score!",
+  description:
+    "Optimize your fitness journey with our Air Force PT Score Calculator web app! Tailored for USAF personnel, this user-friendly tool helps you track your physical fitness progress effortlessly. Accurate, efficient, and easy to use, our calculator ensures you stay on top of your fitness goals. Perfect for airmen stationed worldwide, achieve your best PT scores with precision and confidence. Try it now!",
+  image:
+    "https://res.cloudinary.com/dfpbpun9z/image/upload/v1699624351/OG_slgp7a.png",
 };
 
 export default function RootLayout({
@@ -17,29 +20,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const title = "Calculate your USAF PT Score!";
-  const description =
-    "Optimize your fitness journey with our Air Force PT Score Calculator web app! Tailored for USAF personnel, this user-friendly tool helps you track your physical fitness progress effortlessly. Accurate, efficient, and easy to use, our calculator ensures you stay on top of your fitness goals. Perfect for airmen stationed worldwide, achieve your best PT scores with precision and confidence. Try it now!";
   return (
     <html lang="en" className={`${GeistSans.className} scroll-smooth`}>
       <head>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@imagawaDev" />
         <meta name="twitter:creator" content="@imagawaDev" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta
-          name="twitter:image"
-          content="https://res.cloudinary.com/dfpbpun9z/image/upload/v1699369279/ogimage_oz9omm.png"
-        />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="https://www.usafptcalculator.com/" />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dfpbpun9z/image/upload/v1699369279/ogimage_oz9omm.png"
-        />
-        <meta property="og:description" content={description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:url" content="https://usafptcalculator.com/" />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:description" content={metadata.description} />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🛩</text></svg>"
