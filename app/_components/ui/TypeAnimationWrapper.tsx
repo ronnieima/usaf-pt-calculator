247606e399865fa8083da89b24e202d19ba91113
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-12 17:20:47 +0100

    discover line breaks

--- app/_components/ui/TypeAnimationWrapper.tsx
+++ app/_components/ui/TypeAnimationWrapper.tsx
@@ -6,11 +6,11 @@ const TypeAnimationWrapper = () => {
   return (
     <TypeAnimation
       preRenderFirstString={true}
-      className=" w-[80%] text-center
-        text-5xl text-shadow-lg sm:w-[66%] sm:text-7xl lg:text-8xl"
+      className=" w-[80%] whitespace-pre-line
+        text-center text-5xl text-shadow-lg sm:text-7xl lg:text-8xl	"
       aria-label="Calculate your Air Force PT score."
       role="marquee"
-      sequence={["Cal", "Calculate your Air Force PT score."]}
+      sequence={["Cal", "Calculate your\nAir Force PT score."]}
       wrapper="h1"
       speed={50}
     />
