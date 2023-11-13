e2280c6bf6c28e7f2e6d89907136e03540c00abd
Ronnie <ronnie.imagawa@gmail.com>
2023-11-13 09:56:57 +0100

    add dialog test

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,4 +1,13 @@
+"use client";
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import UnderConstruction from "../_components/UnderConstruction";
 import {
@@ -10,18 +19,26 @@ import {
 
 const ChartsPage = () => {
   return (
-    <>
+    <div>
       <UnderConstruction />
       <Card>
         <CardHeader>
           <CardTitle>&gt;25</CardTitle>
         </CardHeader>
         <CardContent>
-          {/* todo: add this pdf in a dialog */}
-          <iframe src="https://hnnyotwjhikrytqynjyk.supabase.co/storage/v1/object/public/usafptcalculator/scorechart_male_lt25.pdf"></iframe>
+          <Dialog>
+            {/* todo: add this pdf in a dialog */}
+            <DialogTrigger>Open</DialogTrigger>
+            <DialogContent className="min-w-none  min-h-[40rem] ">
+              <iframe
+                src="https://hnnyotwjhikrytqynjyk.supabase.co/storage/v1/object/public/usafptcalculator/scorechart_male_lt25.pdf"
+                className="h-full w-full py-2"
+              ></iframe>
+            </DialogContent>
+          </Dialog>
         </CardContent>
       </Card>
-    </>
+    </div>
   );
 };
 
