f89c7fce9e5ecbace32daa1f7c4793728bbc6706
Ronnie Kaito Imagawa <70875687+ronnieima@users.noreply.github.com>
2023-11-13 11:31:56 +0100

    Add initial charts page route (#30)
    * Update README.md
    
    * clean up braces
    
    * start on charts
    
    * add dialog test
    
    * try link
    
    * finish pt chart draft
    
    * fix mobile layout

--- app/_components/ui/(form)/(score)/ScoreMessage.tsx
+++ app/_components/ui/(form)/(score)/ScoreMessage.tsx
@@ -1,15 +1,12 @@
 import ScoreIcons from "./ScoreIcons";
 import ScoreText from "./ScoreText";
-import ScoreStatus from "./ScoreStatus";
 
 function ScoreMessage() {
   return (
-    <>
-      <section className="space-y-16 sm:grid sm:grid-cols-2">
-        <ScoreText />
-        <ScoreIcons />
-      </section>
-    </>
+    <section className="space-y-16 sm:grid sm:grid-cols-2">
+      <ScoreText />
+      <ScoreIcons />
+    </section>
   );
 }
 
--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,11 +1,60 @@
-import React from "react";
-import { Button } from "../_components/ui/(shadcn)/button";
-import { ChevronRight } from "lucide-react";
+"use client";
+
 import Link from "next/link";
-import UnderConstruction from "../_components/UnderConstruction";
+import { ageGroups } from "../_components/ui/(form)/(controls)/AgeGroupSelect";
+import { Card, CardHeader, CardTitle } from "../_components/ui/(shadcn)/card";
+
+const genders = ["male", "female"];
+const ages = ageGroups;
 
 const ChartsPage = () => {
-  return <UnderConstruction />;
+  function formatAgeGroup(ageGroup: string) {
+    switch (ageGroup) {
+      case "<25":
+        return "lt25";
+      case ">60":
+        return "gt60";
+      default:
+        return ageGroup;
+    }
+  }
+  return (
+    <div className="p-16">
+      <h1 className="flex items-center justify-center text-center text-6xl text-background">
+        Score Charts
+      </h1>
+      <div className="grid min-h-screen gap-8 py-16 md:grid-cols-2 lg:grid-cols-3 lg:px-32 ">
+        {genders.map((gender) =>
+          ages.map((ageGroup) => {
+            console.log(ageGroup);
+            const ageGroupFormatted = formatAgeGroup(ageGroup);
+            return (
+              <Link
+                key={`${gender}-${ageGroup}`}
+                href={`https://hnnyotwjhikrytqynjyk.supabase.co/storage/v1/object/public/usafptcalculator/scorechart_${gender}_${ageGroupFormatted}.pdf`}
+                target="_blank"
+              >
+                <Card
+                  className={`flex h-32 items-center justify-center rounded-none shadow-2xl hover:scale-105 active:translate-y-1 ${
+                    gender === "male"
+                      ? "bg-blue-100 hover:bg-blue-200"
+                      : "bg-pink-100 hover:bg-pink-200"
+                  }`}
+                >
+                  <CardHeader>
+                    <CardTitle className="flex flex-col items-center justify-center text-xl capitalize">
+                      <p>{gender}</p>
+                      <p>{ageGroup}</p>
+                    </CardTitle>
+                  </CardHeader>
+                </Card>
+              </Link>
+            );
+          }),
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ChartsPage;
