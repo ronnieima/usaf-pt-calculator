881f52089794d625f7936b3e5929fb0ac5a28372
Ronnie <ronnie.imagawa@gmail.com>
2023-11-13 08:52:32 +0100

    start on charts

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Button } from "../_components/ui/(shadcn)/button";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
+
 import UnderConstruction from "../_components/UnderConstruction";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../_components/ui/(shadcn)/card";
 
 const ChartsPage = () => {
-  return <UnderConstruction />;
+  return (
+    <>
+      <UnderConstruction />
+      <Card>
+        <CardHeader>
+          <CardTitle>&gt;25</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* todo: add this pdf in a dialog */}
+          <iframe src="https://hnnyotwjhikrytqynjyk.supabase.co/storage/v1/object/public/usafptcalculator/scorechart_male_lt25.pdf"></iframe>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default ChartsPage;
