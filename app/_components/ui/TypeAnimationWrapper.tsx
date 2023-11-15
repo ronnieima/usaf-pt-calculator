43b422f07779decc4eddea148a703aef26e8f2f3
Ronnie Kaito Imagawa <70875687+ronnieima@users.noreply.github.com>
2023-11-15 14:19:17 +0100

    Improve typing animation

--- app/_components/ui/TypeAnimationWrapper.tsx
+++ app/_components/ui/TypeAnimationWrapper.tsx
@@ -4,15 +4,30 @@ import { TypeAnimation } from "react-type-animation";
 
 const TypeAnimationWrapper = () => {
   return (
-    <TypeAnimation
-      preRenderFirstString={true}
-      className=" w-[80%] whitespace-pre-line
-        text-center text-5xl text-shadow-lg sm:text-7xl lg:text-8xl	"
-      aria-label="Calculate your Air Force PT score."
-      sequence={["Cal", "Calculate your\nAir Force PT score."]}
-      wrapper="h1"
-      speed={50}
-    />
+    <h1 className="relative text-center">
+      <span
+        style={{
+          clip: "rect(1px, 1px, 1px, 1px)",
+          clipPath: "inset(50%)",
+          height: "1px",
+          width: "1px",
+          margin: "-1px",
+          overflow: "hidden",
+          padding: "0",
+          position: "absolute",
+        }}
+      >
+        Calculate your Air Force PT score.
+      </span>
+      <TypeAnimation
+        aria-hidden="true"
+        preRenderFirstString={true}
+        className=" w-[80%] whitespace-pre-line
+       text-5xl text-shadow-lg sm:text-7xl lg:text-8xl	"
+        sequence={["Cal", "Calculate your\nAir Force PT score."]}
+        wrapper="span"
+      />
+    </h1>
   );
 };
 
