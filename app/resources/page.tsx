6ad0977ccd7968a89c44ee741e414b6e43edca10
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-18 09:21:46 +0100

    add dafman

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import UnderConstruction from "../_components/UnderConstruction";
 
 const ResourcesPage = () => {
-  return <UnderConstruction />;
+  return (
+    <div className=" h-screen px-8 py-8 lg:px-64">
+      <iframe
+        className="h-full w-full"
+        src="https://res.cloudinary.com/dfpbpun9z/image/upload/v1700294852/dafman36-2905.pdf"
+      ></iframe>
+    </div>
+  );
 };
 
 export default ResourcesPage;
