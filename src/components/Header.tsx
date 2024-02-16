66ec292b6dd11722fb17143e18a2a8b76bc78c87
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-02-16 16:41:30 +0100

    add gradient to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,17 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="flex animate-fade-down flex-col items-center justify-center gap-8 px-2 py-20 text-center sm:px-16 md:py-48">
+    <header className="animate-fade-down flex flex-col items-center justify-center gap-8 px-2 py-20 text-center sm:px-16 md:py-48">
       <h1
         className="
-       text-7xl font-bold tracking-tight text-shadow-lg lg:text-8xl"
+       text-shadow-lg text-7xl font-bold tracking-tight lg:text-8xl"
       >
         Calculate your
         <br />
-        <span className="text-sky-500">Air Force</span> PT score.
+        <span className="bg-gradient-to-r from-sky-400 to-violet-300 bg-clip-text text-transparent">
+          Air Force
+        </span>{' '}
+        PT score.
       </h1>
       <p className="max-w-prose text-lg font-light leading-tight text-gray-300">
         Your personal one-stop shop for Air Force fitness test resources.
